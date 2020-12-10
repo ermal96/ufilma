68ed2b2e81ada1ee990c83598bf10eb2179fe76f
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2020-12-10 12:12:42 +0100

    remove template enngine

--- server/config/config.js
+++ server/config/config.js
@@ -1,19 +1,17 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import cors from "cors";
-import  bodyParser  from "body-parser";
+import bodyParser from "body-parser";
 
 export const config = (app) => {
-  dotenv.config()
+  dotenv.config();
 
   const port = 5000;
-  
+
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(cors());
-  app.set('view engine', 'pug')
 
   app.listen(port, () => {
     console.log(`Server is listening at http://127.0.0.1:${port}/`);
   });
 };
-
