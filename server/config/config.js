7dd1e976c10979b2024728e05f570db1217b8dba
root <root@vmi489031.contaboserver.net>
2020-12-10 09:35:15 +0100

    fix port

--- server/config/config.js
+++ server/config/config.js
@@ -1,22 +1,12 @@
 const config = (app) => {
   require("dotenv").config();
   const bodyParser = require("body-parser");
-  const port = 8080;
+  const port = 5000;
   const cors = require("cors");
 
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(cors());
-
-  // app.use((req, res, next) => {
-  //   const apiKey = req.get("key");
-
-  //   if (apiKey !== process.env.API_KEY) {
-  //     res.status(401).json({ message: "unauthorised" });
-  //   } else {
-  //     next();
-  //   }
-  // });
 
   app.listen(port, () => {
     console.log(`Server is listening at http://127.0.0.1:${port}/`);
