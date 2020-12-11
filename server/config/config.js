173d842fafa4e255ab458dd6541f98c6226a1bdd
root <root@vmi489031.contaboserver.net>
2020-12-11 17:05:28 +0100

    export __dirname

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
+import path from 'path';
 
 export const config = (app) => {
   dotenv.config();
@@ -11,7 +12,11 @@ export const config = (app) => {
   app.use(bodyParser.json());
   app.use(cors());
 
+
   app.listen(port, () => {
     console.log(`Server is listening at http://127.0.0.1:${port}/`);
   });
 };
+
+
+export const __dirname = path.resolve(path.dirname(decodeURI(new URL(import.meta.url).pathname)));
