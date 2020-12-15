c0c1b6784716ddee0e8317a52b9719f3765a9da9
Ermal Vrapi <ermalvrapi18@gmail.com>
2020-12-15 20:16:17 +0100

    fix

--- cms/src/containers/Login/Login.js
+++ cms/src/containers/Login/Login.js
@@ -1,18 +1,8 @@
 import React from "react";
 import { Container } from "reactstrap";
 import { LoginForm } from "../../components/Login";
-// import { useDispatch } from "react-redux";
-// import { fetchUser } from "../../store/actions/userActions";
 
 const Login = () => {
-  // const dispatch = useDispatch();
-
-  const onFinish = (e) => {
-    e.preventDefault();
-
-    // dispatch(fetchUser());
-  };
-
   return (
     <Container>
       <LoginForm />
