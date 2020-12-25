1ac0fdbea9cb0891fe3e37b6434928e3443c355b
ERmal Vrapi <ermalvrapi18@gmail.com>
2020-12-25 14:19:43 +0100

    fix clien name

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export const axiosConfig = () => {
   const token = localStorage.getItem("token");
-  axios.defaults.baseURL = "https://ufilma.com/api";
+  // https://ufilma.com/api
+  axios.defaults.baseURL = "http://127.0.0.1:5000/api";
 
   axios.defaults.headers.common["authorization"] = token;
 };
