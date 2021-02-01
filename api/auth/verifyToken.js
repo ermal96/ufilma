546b1aa0a7668819a91426cc833920d64326aa1b
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2021-02-01 15:01:14 +0100

    fixes

--- api/auth/verifyToken.js
+++ api/auth/verifyToken.js
@@ -5,17 +5,12 @@ dotenv.config();
 
 export const verifyToken = (req, res, next) => {
   const token = req.headers["authorization"];
-  if (!token)
-    return res.status(401).send({ auth: false, message: "No token provided." });
+  if (!token || token === null) return res.status(401).send({ auth: false, message: "No token provided." });
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err)
-      return res
-        .status(500)
-        .send({ auth: false, message: "Failed to authenticate token." });
+    if (err) return res.status(500).send({ auth: false, message: "Failed to authenticate token." });
 
     req.user = decoded;
     next();
-   
   });
 };
