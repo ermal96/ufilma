d01243c23c49a55ba302e795edcaa326e5ea7d04
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2021-02-03 15:33:28 +0100

    fixes

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -19,5 +18,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-serviceWorkerRegistration.register();
