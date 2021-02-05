33a68c50bcc99896a4a297e3c10f9991b28c7028
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2021-02-05 14:13:39 +0100

    fixes

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,10 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { config } from "./config";
-import ReactGA from "react-ga";
 import "swiper/swiper.scss";
 import "./styles/index.scss";
 
 config();
-
-ReactGA.initialize("UA-59YYQCQ42C");
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <Provider store={store}>
