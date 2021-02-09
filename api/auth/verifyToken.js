84c3b45ab52fe3869ddf6278d201b63c53e4a074
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2021-02-09 14:53:32 +0100

    fixes

--- api/auth/verifyToken.js
+++ api/auth/verifyToken.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 export const verifyToken = (req, res, next) => {
   const token = req.headers["authorization"];
-  if (!token || token === null) return res.status(401).send({ auth: false, message: "No token provided." });
+  if (!token || token === null) return res.status(401).send({ auth: false, message: "Nuk keni akses per kte veprim" });
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.status(500).send({ auth: false, message: "Failed to authenticate token." });
+    if (err) return res.status(500).send({ auth: false, message: "Kredincialet nuk jane te sakta." });
 
     req.user = decoded;
     next();
